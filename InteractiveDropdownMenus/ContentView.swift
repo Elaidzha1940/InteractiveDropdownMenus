df9f40c5636460c9426a2901a57b82bcf2e39817
Elaidzha1940 <eli080293@gmail.com>
2023-10-14 21:00:20 +0300

    Fix Bg Color

--- InteractiveDropdownMenus/ContentView.swift
+++ InteractiveDropdownMenus/ContentView.swift
@@ -14,34 +14,40 @@ struct ContentView: View {
     
     var body: some View {
         
-        VStack {
-            ForEach(items) { item in
-                HStack {
-                    Image(systemName: item.icon)
-                        .font(.system(size: 18, weight: .bold, design: .rounded))
-                        .padding(10)
-                        .background(.ultraThinMaterial, in: Circle())
-                    
-                    Text(item.title)
-                        .font(.system(size: 18, weight: .bold, design: .rounded))
-                    
-                    Spacer()
-                    
-                    Image(systemName: "chevron.forward")
-                        .font(.system(size: 18, weight: .bold, design: .rounded))
+        ZStack {
+            Color(Color("BG"))
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                ForEach(items) { item in
+                    HStack {
+                        Image(systemName: item.icon)
+                            .font(.system(size: 18, weight: .bold, design: .rounded))
+                            .padding(10)
+                            .background(.ultraThinMaterial, in: Circle())
+                        
+                        Text(item.title)
+                            .font(.system(size: 18, weight: .bold, design: .rounded))
+                            .foregroundColor(.white.opacity(0.6))
+                        
+                        Spacer()
+                        
+                        Image(systemName: "chevron.forward")
+                            .font(.system(size: 18, weight: .bold, design: .rounded))
+                            .foregroundColor(.white.opacity(0.6))
+                    }
+                    .padding(.horizontal)
+                    .frame(width: idDropdownOpen ? 350 : 350 - CGFloat((items.count - 1 - items.firstIndex(where: {$0.id == item.id})!) * 10), height: 70)
+                    .background(Color("BG"))
+                    .cornerRadius(20)
+                    .offset(y: idDropdownOpen ? 0 : CGFloat(-70 * items.firstIndex(where: { $0.id == item.id })!))
+                    .shadow(color: .black.opacity(0.5), radius: 1, x: 2, y: -3)
                 }
-                .padding(.horizontal)
-                .frame(width: idDropdownOpen ? 300 : 300 - CGFloat((items.count - 1 - items.firstIndex(where: {$0.id == item.id})!) * 10), height: 70)
-                .background(Color.gray)
-                .cornerRadius(20)
-                .offset(y: idDropdownOpen ? 0 : CGFloat(-70 * items.firstIndex(where: { $0.id == item.id })!))
-                .shadow(color: .white, radius: 1, x: 0, y: -3)
             }
-        }
-        .preferredColorScheme(.dark)
-        .onTapGesture {
-            withAnimation(.interactiveSpring(response: 0.8, dampingFraction: 0.6)) {
-                idDropdownOpen.toggle()
+            .onTapGesture {
+                withAnimation(.interactiveSpring(response: 0.8, dampingFraction: 0.6)) {
+                    idDropdownOpen.toggle()
+                }
             }
         }
     }
@@ -61,7 +67,7 @@ struct DataItem: Identifiable {
 let items: [DataItem] = [
     
     DataItem(icon: "play.fill", title: "Play"),
-    DataItem(icon: "pause.circle.fill", title: "Pause"),
-    DataItem(icon: "repeat.circle.fill", title: "Repeat"),
+    DataItem(icon: "stop.fill", title: "Stop"),
+    DataItem(icon: "backward.end.fill", title: "Back"),
     DataItem(icon: "forward.end.fill", title: "Forward"),
 ]
