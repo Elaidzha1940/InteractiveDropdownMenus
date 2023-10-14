8aee3c26527dee6d87ae578f81b7b3079f4cdd5f
Elaidzha1940 <eli080293@gmail.com>
2023-10-14 20:24:45 +0300

    Add DataItem

--- InteractiveDropdownMenus/ContentView.swift
+++ InteractiveDropdownMenus/ContentView.swift
@@ -13,25 +13,26 @@ struct ContentView: View {
     var body: some View {
         
         VStack {
-            HStack {
-              
-                Image(systemName: "xmark")
-                    .font(.system(size: 18, weight: .bold, design: .rounded))
-                    .padding(10)
-                    .background(.ultraThinMaterial, in: Circle())
-                
-                Text("title")
-                    .font(.system(size: 18, weight: .bold, design: .rounded))
-                
-                Spacer()
-                
-                Image(systemName: "chevron.forward")
-                    .font(.system(size: 18, weight: .bold, design: .rounded))
-            }
-            .padding(.horizontal)
-            .frame(width: 300, height: 70)
-            .background(Color.gray)
+            ForEach(items) { item in
+                HStack {
+                    Image(systemName: item.icon)
+                        .font(.system(size: 18, weight: .bold, design: .rounded))
+                        .padding(10)
+                        .background(.ultraThinMaterial, in: Circle())
+                    
+                    Text(item.title)
+                        .font(.system(size: 18, weight: .bold, design: .rounded))
+                    
+                    Spacer()
+                    
+                    Image(systemName: "chevron.forward")
+                        .font(.system(size: 18, weight: .bold, design: .rounded))
+                }
+                .padding(.horizontal)
+                .frame(width: 300, height: 70)
+                .background(Color.gray)
             .cornerRadius(20)
+            }
         }
         .preferredColorScheme(.dark)
     }
@@ -40,3 +41,18 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
+struct DataItem: Identifiable {
+    
+    var id = UUID()
+    var icon: String
+    var title: String
+}
+
+let items: [DataItem] = [
+
+    DataItem(icon: "play.fill", title: "Play"),
+    DataItem(icon: "pause.circle.fill", title: "Pause"),
+    DataItem(icon: "repeat.circle.fill", title: "Repeat"),
+    DataItem(icon: "forward.end.fill", title: "Forward"),
+]
