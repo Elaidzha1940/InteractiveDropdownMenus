1eccaea70070fdd607a102f3984286ff62d2138a
Elaidzha1940 <eli080293@gmail.com>
2023-10-14 20:04:27 +0300

    InteractiveDropdownMenus

---
+++ InteractiveDropdownMenus/ContentView.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: InteractiveDropdownMenus
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 14.10.2023
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+          
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InteractiveDropdownMenus/InteractiveDropdownMenusApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: InteractiveDropdownMenus
+//  File: InteractiveDropdownMenusApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 14.10.2023
+//
+//  */
+
+import SwiftUI
+
+@main
+struct InteractiveDropdownMenusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
