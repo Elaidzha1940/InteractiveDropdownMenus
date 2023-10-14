38b75eec966979bca61d52cf933bc76a614faea8
Elaidzha1940 <eli080293@gmail.com>
2023-10-14 20:13:57 +0300

    Updated ContentView

--- InteractiveDropdownMenus/ContentView.swift
+++ InteractiveDropdownMenus/ContentView.swift
@@ -13,8 +13,27 @@ struct ContentView: View {
     var body: some View {
         
         VStack {
-          
+            HStack {
+              
+                Image(systemName: "xmark")
+                    .font(.system(size: 18, weight: .bold, design: .rounded))
+                    .padding(10)
+                    .background(.ultraThinMaterial, in: Circle())
+                
+                Text("title")
+                    .font(.system(size: 18, weight: .bold, design: .rounded))
+                
+                Spacer()
+                
+                Image(systemName: "chevron.forward")
+                    .font(.system(size: 18, weight: .bold, design: .rounded))
+            }
+            .padding(.horizontal)
+            .frame(width: 300, height: 70)
+            .background(Color.gray)
+            .cornerRadius(20)
         }
+        .preferredColorScheme(.dark)
     }
 }
 
